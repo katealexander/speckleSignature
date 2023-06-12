8a79a48576320627634785a785488bc930f26c05
Katherine Alexander <katalex115@gmail.com>
2023-06-12 12:18:53 -0400

    updating Graph_indivdual_genes

--- Graph_individual_genes/HIFresponsiveGenePlots.R
+++ Graph_individual_genes/HIFresponsiveGenePlots.R
@@ -44,20 +44,22 @@ dev.off()
 
 ## cluster compare GO analysis of I-biased and II-biased HIF2A responsive genes, including ns
 B$log2Ratio <- log2(B$SigI/B$SigII)
-Igenes <- B$Gene[B$log2Ratio > 0 & B$pIvsII < 0.05]
-IIgenes <- B$Gene[B$log2Ratio < 0 & B$pIvsII < 0.05]
-# unchangingGenes <- B$Gene[B$log2Ratio < 0.15 & B$log2Ratio > -0.15 & B$pIvsII > 0.1]
-unchangingGenes <- B$Gene[B$pIvsII > 0.1]
+Igenes <- B$Gene[B$log2Ratio > 0 & B$pIvsII < 0.005]
+IIgenes <- B$Gene[B$log2Ratio < 0 & B$pIvsII < 0.005]
+unchangingGenes <- B$Gene[B$log2Ratio < 0.15 & B$log2Ratio > -0.15 & B$pIvsII > 0.05]
+# unchangingGenes <- B$Gene[B$pIvsII > 0.1]
 Igenes.df <- bitr(Igenes, fromType = "SYMBOL", toType = c("ENSEMBL", "ENTREZID"), OrgDb = org.Hs.eg.db)
 IIgenes.df <- bitr(IIgenes, fromType = "SYMBOL", toType = c("ENSEMBL", "ENTREZID"), OrgDb = org.Hs.eg.db)
 unchangingGenes.df <- bitr(unchangingGenes, fromType = "SYMBOL", toType = c("ENSEMBL", "ENTREZID"), OrgDb = org.Hs.eg.db)
 geneList <- list(I=Igenes.df$ENTREZID, ns=unchangingGenes.df$ENTREZID, II=IIgenes.df$ENTREZID)
 y=compareCluster(geneList, fun = "enrichGO", ont = "BP", OrgDb = org.Hs.eg.db)
-Y <- simplify(y, cutoff=0.4,)
-p = dotplot(Y, showCategory = 8)
+Y <- simplify(y, cutoff=0.5,)
+p = dotplot(Y, showCategory =8)
 filename = "clusterProfiler_IvsIIbias_withNS.pdf"
-pdf(filename, width = 6, height = 6.5, onefile=FALSE)
+pdf(filename, width = 6, height = 7, onefile=FALSE)
 print(p)
 dev.off()
+
+# cnetplot(Y)
 
 
